26b832150779beeadb2b009e0e696a3a6a2779d4
Dionna Glaze <dionnaglaze@google.com>
2025-06-04 16:59:29 +0000

    virt/coco/tsm: Add service_error RO attribute
    A service provider may return an error code different from -EINVAL when
    requesting a manifestblob, so make that result code readable.
    
    Given that different services may report errors differently, add a new
    provider operation, service_error_show, which will be called when the
    attribute is visible, is requested to be shown, and the service error
    is non-NULL.
    
    A NULL value for the service error results in an empty service_error
    attribute.
    
    The AMD SVSM attestation protocol specifies protocol-specific result
    codes to be within the 0x8000_1000-0xFFFF_FFFF range.
    
    Signed-off-by: Dionna Glaze <dionnaglaze@google.com>

--- drivers/virt/coco/tsm.c
+++ drivers/virt/coco/tsm.c
@@ -296,9 +296,11 @@ static ssize_t tsm_report_read(struct tsm_report *report, void *buf,
 	kvfree(report->outblob);
 	kvfree(report->auxblob);
 	kvfree(report->manifestblob);
+	kfree(report->service_error);
 	report->outblob = NULL;
 	report->auxblob = NULL;
 	report->manifestblob = NULL;
+	report->service_error = NULL;
 	rc = ops->report_new(report, provider.data);
 	if (rc < 0)
 		return rc;
@@ -334,6 +336,29 @@ static ssize_t tsm_report_manifestblob_read(struct config_item *cfg, void *buf,
 }
 CONFIGFS_BIN_ATTR_RO(tsm_report_, manifestblob, NULL, TSM_OUTBLOB_MAX);
 
+static ssize_t default_service_error_show(char *buf, void *service_error)
+{
+	if (service_error)
+		return sysfs_emit(buf, "unspecified error\n");
+
+	/* No error implies empty error file. */
+	return 0;
+}
+
+static ssize_t tsm_report_service_error_show(struct config_item *cfg,
+					     char *buf)
+{
+	struct tsm_report *report = to_tsm_report(cfg);
+	void *service_error = report->service_error;
+	ssize_t (*show)(char *, void*) = provider.ops->service_error_show;
+
+	if (!show || !service_error)
+		show = default_service_error_show;
+
+	return show(buf, service_error);
+}
+CONFIGFS_ATTR_RO(tsm_report_, service_error);
+
 static struct configfs_attribute *tsm_report_attrs[] = {
 	[TSM_REPORT_GENERATION] = &tsm_report_attr_generation,
 	[TSM_REPORT_PROVIDER] = &tsm_report_attr_provider,
@@ -342,6 +367,7 @@ static struct configfs_attribute *tsm_report_attrs[] = {
 	[TSM_REPORT_SERVICE_PROVIDER] = &tsm_report_attr_service_provider,
 	[TSM_REPORT_SERVICE_GUID] = &tsm_report_attr_service_guid,
 	[TSM_REPORT_SERVICE_MANIFEST_VER] = &tsm_report_attr_service_manifest_version,
+	[TSM_REPORT_SERVICE_ERROR] = &tsm_report_attr_service_error,
 	NULL,
 };
 
@@ -362,6 +388,7 @@ static void tsm_report_item_release(struct config_item *cfg)
 	kvfree(report->auxblob);
 	kvfree(report->outblob);
 	kfree(report->desc.service_provider);
+	kfree(report->service_error);
 	kfree(state);
 }
 
--- include/linux/tsm.h
+++ include/linux/tsm.h
@@ -42,6 +42,9 @@ struct tsm_desc {
  * @auxblob: (optional) auxiliary data to the report (e.g. certificate data)
  * @manifestblob_len: sizeof(@manifestblob)
  * @manifestblob: (optional) manifest data associated with the report
+ * @service_error: (optional) a service-specific error report encountered when
+ *   using service_provider. Type and interpretation are up to the provider
+ *   operation service_error_show.
  */
 struct tsm_report {
 	struct tsm_desc desc;
@@ -51,6 +54,7 @@ struct tsm_report {
 	u8 *auxblob;
 	size_t manifestblob_len;
 	u8 *manifestblob;
+	void *service_error;
 };
 
 /**
@@ -62,6 +66,8 @@ struct tsm_report {
  * @TSM_REPORT_SERVICE_PROVIDER: index of the service provider identifier attribute
  * @TSM_REPORT_SERVICE_GUID: index of the service GUID attribute
  * @TSM_REPORT_SERVICE_MANIFEST_VER: index of the service manifest version attribute
+ * @TSM_REPORT_SERVICE_ERROR: a service-specific error report encountered when using
+ *   service_provider.
  */
 enum tsm_attr_index {
 	TSM_REPORT_GENERATION,
@@ -71,6 +77,7 @@ enum tsm_attr_index {
 	TSM_REPORT_SERVICE_PROVIDER,
 	TSM_REPORT_SERVICE_GUID,
 	TSM_REPORT_SERVICE_MANIFEST_VER,
+	TSM_REPORT_SERVICE_ERROR,
 };
 
 /**
@@ -92,9 +99,16 @@ enum tsm_bin_attr_index {
  * @name: tsm id reflected in /sys/kernel/config/tsm/report/$report/provider
  * @privlevel_floor: convey base privlevel for nested scenarios
  * @report_new: Populate @report with the report blob and auxblob
- * (optional), return 0 on successful population, or -errno otherwise
+ * (optional), return 0 on successful population, or -errno otherwise.
+ *   Service-specific errors should return -EIO and populate the service_error
+ *   object to report with service_error_show. A NULL service_error is treated
+ *   as success.
  * @report_attr_visible: show or hide a report attribute entry
  * @report_bin_attr_visible: show or hide a report binary attribute entry
+ * @service_error_show: Populate @buf with human readable description of
+ *   an error encountered for service-specific report_new behavior. The
+ *   @service_error is expected to be created by report_new and freeable
+ *   with kfree. This is only called when @service_error is non-NULL.
  *
  * Implementation specific ops, only one is expected to be registered at
  * a time i.e. only one of "sev-guest", "tdx-guest", etc.
@@ -105,6 +119,7 @@ struct tsm_ops {
 	int (*report_new)(struct tsm_report *report, void *data);
 	bool (*report_attr_visible)(int n);
 	bool (*report_bin_attr_visible)(int n);
+	ssize_t (*service_error_show)(char *buf, void *service_error);
 };
 
 int tsm_register(const struct tsm_ops *ops, void *priv);
